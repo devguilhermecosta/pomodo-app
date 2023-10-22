469f1b89cce9253a09a30097049996a30ef4d2db
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-10-22 19:55:32 -0300

    update: imported the zeroLeft

--- src/utils/seconds-to-minute.ts
+++ src/utils/seconds-to-minute.ts
@@ -1,5 +1,6 @@
+import { zeroLeft } from './zero-left';
+
 export function secondsToMinute(seconds: number): string {
-  const zeroLeft = (n: number) => Math.floor(n).toString().padStart(2, '0');
   const min = zeroLeft((seconds / 60) % 60);
   const sec = zeroLeft((seconds % 60) % 60);
   return `${min}:${sec}`;
